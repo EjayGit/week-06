3ad1daecfbe0ebef2edb297a762513a728cc083c
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2026-01-09 15:31:56 +0000

    aria-labels added

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ export default function App() {
   return (
     <div class='body'>
       <GlobalKeyPress setCurrentImage={setCurrentImage} currentImage={currentImage} images={images}/>
-      <DecrementButton images={images} currentImage={currentImage} setCurrentImage={setCurrentImage}/>
-      <IncrementButton images={images} currentImage={currentImage} setCurrentImage={setCurrentImage}/>
-      <Thumbnails images={images} setImages={setImages} setCurrentImage={setCurrentImage}/>
-      {images.length > 0 && (<img className='image-fullscreen' src={images[currentImage].url} alt={'An image of a cat'} />)}
+      <DecrementButton aria-label={'slide image left'} images={images} currentImage={currentImage} setCurrentImage={setCurrentImage}/>
+      <IncrementButton aria-label={'slide image right'} images={images} currentImage={currentImage} setCurrentImage={setCurrentImage}/>
+      <Thumbnails aria-label={'thumbnail images'} images={images} setImages={setImages} setCurrentImage={setCurrentImage}/>
+      {images.length > 0 && (<img aria-label={'main image'} className='image-fullscreen' src={images[currentImage].url} alt={'An image of a cat'} />)}
     </div>
   )
 }
